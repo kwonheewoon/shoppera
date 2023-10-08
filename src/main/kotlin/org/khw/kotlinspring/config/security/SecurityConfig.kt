1a06826a91debc620becf66feb37d4bbe93c912e
kwonheewoon <gmldns2486@gmail.com>
2023-10-08 21:00:35 +0900

    시큐리티 Jwt 필터 적용

--- src/main/kotlin/org/khw/kotlinspring/config/security/SecurityConfig.kt
+++ src/main/kotlin/org/khw/kotlinspring/config/security/SecurityConfig.kt
@@ -1,6 +1,7 @@
 package org.khw.kotlinspring.config.security
 
 import lombok.RequiredArgsConstructor
+import org.khw.kotlinspring.config.security.filter.JwtAuthenticationFilter
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,11 +10,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 import org.springframework.security.web.csrf.CsrfFilter
 
 @Configuration
 @RequiredArgsConstructor
 class SecurityConfig(
+    val jwtAuthenticationFilter: JwtAuthenticationFilter,
     val customAuthenticationProvider: CustomAuthenticationProvider,
     val customAuthenticationSuccessHandler: CustomAuthenticationSuccessHandler,
     val customAuthenticationFailureHandler: CustomAuthenticationFailureHandler
@@ -23,33 +26,28 @@ class SecurityConfig(
     fun filterChain(http: HttpSecurity) : SecurityFilterChain{
 
         val authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder::class.java)
-//        authenticationManagerBuilder.inMemoryAuthentication()
-//            .withUser("john")
-//            .password("12345")
-//            .authorities("read")
-//            .and()
-//            .passwordEncoder(NoOpPasswordEncoder.getInstance())
-        authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider)
+
+        //authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider)
         val authenticationManager = authenticationManagerBuilder.build();
 
         http.csrf().disable()
         http.headers().frameOptions().disable()
 
 
-        http.formLogin()
-            .defaultSuccessUrl("/home", true)
-            .successHandler(customAuthenticationSuccessHandler)
-            .failureHandler(customAuthenticationFailureHandler)
-            .and()
-        http.httpBasic { c ->
-            c.realmName("OTHER")
-            c.authenticationEntryPoint(CustomEntryPoint())
-        }
+//        http.formLogin()
+//            .defaultSuccessUrl("/home", true)
+//            .successHandler(customAuthenticationSuccessHandler)
+//            .failureHandler(customAuthenticationFailureHandler)
+//            .and()
+//        http.httpBasic { c ->
+//            c.realmName("OTHER")
+//            c.authenticationEntryPoint(CustomEntryPoint())
+//        }
 
 
 
         http
-            //.addFilterAfter(CsrfTokenLogger(), CsrfFilter::class.java)
+            .addFilterAt(jwtAuthenticationFilter, BasicAuthenticationFilter::class.java)
             .authorizeHttpRequests()
             .requestMatchers(PathRequest.toH2Console()).permitAll()
             .requestMatchers(HttpMethod.POST, "/user").permitAll()
